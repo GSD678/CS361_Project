6fdc64335d3512af8a7865ae78135716e6776a0e
Karim <kimo_678@live.com>
2023-12-04 15:14:23 -0500

    updated title

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -14,3 +14,20 @@ export default defineConfig({
   integrations: [tailwind(), svelte(), react(), autoImport()],
   renderers: ['@astrojs/node', 'auth-astro']
 });
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
