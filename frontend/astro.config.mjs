9e1b1a59dba8915f7b0c44f6274e99f8d8aff7eb
Karim <kimo_678@live.com>
2023-12-04 15:17:54 -0500

    adjusted config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -12,22 +12,12 @@ export default defineConfig({
   adapter: vercel(),
   output: 'server',
   integrations: [tailwind(), svelte(), react(), autoImport()],
-  renderers: ['@astrojs/node', 'auth-astro']
+  renderers: ['@astrojs/node', 'auth-astro'],
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "" },
+    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept,Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  ],
+  source:  "/api/:path"
 });
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept,Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
